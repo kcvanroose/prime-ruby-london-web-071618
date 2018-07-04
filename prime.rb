996d8ef6f36b7cc69cb73fff37c64b4bb95fa133
casey van-roose <kcvanroose@gmail.com>
2018-07-04 15:29:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 def prime?(num)
   return false if num <= 1
-  squared_num = Math.sqrt(num)num.downto(2).each {|i| return false if num % i == 0}
+  squared_num = Math.sqrt(num)
+  squared_num.downto(2).each {|i| return false if num % i == 0}
   true
 end
